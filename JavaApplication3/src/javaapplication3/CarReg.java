4502cde70250119cf62cf097620fc5e1bae5abdc
Lowtechdolphin <ITDSIU22173@student.hcmiu.edu.vn>
2024-05-09 14:59:59 +0700

    CarReg

--- JavaApplication3/src/javaapplication3/CarReg.java
+++ JavaApplication3/src/javaapplication3/CarReg.java
@@ -2,17 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+package javaapplication3;
 
 /**
  *
  * @author NGUYEN LIN
  */
-public class Main extends javax.swing.JFrame {
+public class CarReg extends javax.swing.JFrame {
 
     /**
-     * Creates new form Main
+     * Creates new form CarReg
      */
-    public Main() {
+    public CarReg() {
         initComponents();
     }
 
@@ -58,20 +59,20 @@ public class Main extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CarReg.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CarReg.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CarReg.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CarReg.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Main().setVisible(true);
+                new CarReg().setVisible(true);
             }
         });
     }
